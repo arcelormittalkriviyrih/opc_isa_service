f2fbb858693cc444941c49a2fd62ba6b700ea169
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-29 15:37:00 +0300

    execute jobs in order of creation

--- ServicesLib/DbDataClassLib.cs
+++ ServicesLib/DbDataClassLib.cs
@@ -121,7 +121,7 @@ namespace JobOrdersService
         public JobOrders(string webServiceUrl, string workType, string dispatchStatus)
         {
             this.webServiceUrl = webServiceUrl;
-            string JobOrdersUrl = Requests.CreateRequest(webServiceUrl, "v_JobOrders?$filter=WorkType%20eq%20%27" + workType + "%27%20and%20DispatchStatus%20eq%20%27" + dispatchStatus + "%27&$select=ID,Command,CommandRule");
+            string JobOrdersUrl = Requests.CreateRequest(webServiceUrl, "v_JobOrders?$filter=WorkType%20eq%20%27" + workType + "%27%20and%20DispatchStatus%20eq%20%27" + dispatchStatus + "%27&$orderby=ID&$select=ID,Command,CommandRule");
             //test string JobOrdersUrl = Requests.CreateRequest(webServiceUrl, "v_JobOrders?$filter=ID%20eq%2040625&$select=ID,Command,CommandRule"); 
             string JobOrdersSerial = Requests.MakeRequest(JobOrdersUrl);
             jobOrdersObj = DeserializeJobOrders(JobOrdersSerial);
