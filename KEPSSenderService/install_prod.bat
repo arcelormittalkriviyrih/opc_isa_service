09a55f9fa398f89dc46c7eeb6328aa593ab38f16
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2017-04-20 08:50:17 +0300

    enable default run service

--- KEPSSenderService/install_prod.bat
+++ KEPSSenderService/install_prod.bat
@@ -5,8 +5,8 @@ rem copy new version
 xcopy %WORKSPACE%\KEPSSenderService\bin\Production\*.* \\krr-app-palbp01\Nikama\opc_isa_service /Y
 rem install existing service
 rem echo off
-sc.exe \\krr-app-palbp01 Create "ArcelorMittal.OPCCommandsSender" binPath="C:\Nikama\opc_isa_service\KEPSSenderService.exe" start=demand obj=%ADMIN_USER% password=%ADMIN_PASS%
+sc.exe \\krr-app-palbp01 Create "ArcelorMittal.OPCCommandsSender" binPath="C:\Nikama\opc_isa_service\KEPSSenderService.exe" start=delayed-auto obj=%ADMIN_USER% password=%ADMIN_PASS%
 sc.exe \\krr-app-palbp01 Start "ArcelorMittal.OPCCommandsSender"
 sc.exe \\krr-app-palbp01 Stop "ArcelorMittal.OPCCommandsSender"
 sc.exe \\krr-app-palbp01 config "ArcelorMittal.OPCCommandsSender" obj=%PRINT_USER% password=%PRINT_PASS%
-rem sc.exe \\krr-app-palbp01 Start "ArcelorMittal.OPCCommandsSender"
+sc.exe \\krr-app-palbp01 Start "ArcelorMittal.OPCCommandsSender"
